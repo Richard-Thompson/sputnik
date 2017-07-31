3020f6648b5b9c29f227bcf3ac3bad71915b56ea
Richard-Thompson <richardthompson1990@hotmail.co.uk>
2017-07-31 09:13:37 +0100

    aligning servicesand products section, services and products section completed

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,26 +1,32 @@
 import React, {Component} from 'react';
+import '../css/Services.css';
 
 class Services extends Component {
   render () {
     return (
       <div className="services-wrapper">
-        <p>Services</p>
-        <h1 className="title is-2">Our pledge to quality</h1>
-        <p>We provide education support in countries where the infrastructure is not as established as elsewhere. We seize opportunities and stay strong, even when it gets difficult.</p>
-        <h4>Our aims</h4>
-        <div>
-          <div>
-            <p>To provide free education</p>
-            <p>Empower people</p>
+        <div className="services-content">
+          <p>Services</p>
+          <h1 className="services-title title is-2">Our pledge to quality</h1>
+          <p>We provide education support in countries where the infrastructure is not as established as elsewhere. We seize opportunities and stay strong, even when it gets difficult.</p>
+          <h3 className="services-title title is-3">Our aims</h3>
+          <div className="aims">
+            <div>
+              <p>To provide free education</p>
+              <p>Empower people</p>
+            </div>
+            <div>
+              <p>Help marginalised children</p>
+              <p>Reduce barriers for girls</p>
+            </div>
           </div>
-          <div>
-            <p>Help marginalised children</p>
-            <p>Reduce barriers for girls</p>
+          <div className="services-section">
+            <button className="services-button">ALL SERVICES</button>
+            <p>EXPLORE SERVICES</p>
           </div>
         </div>
-        <div>
-          <button>ALL SERVICES</button>
-          <p>EXPLORE SERVICES</p>
+        <div className="services-image">
+          <img src="images/services.jpg"/>
         </div>
       </div>
     );
